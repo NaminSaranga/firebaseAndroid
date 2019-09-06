23e5a46ac37c23cb1a7f30bf17b4a77a273b6208
NaminSaranga <naminsaranga54@gmail.com>
2019-09-07 00:10:14 +0530

    show,update

--- app/src/main/java/com/sliit/firebasetuteapp/firebasetuteapp/MainActivity.java
+++ app/src/main/java/com/sliit/firebasetuteapp/firebasetuteapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sliit.firebasetuteapp.firebasetuteapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -8,8 +9,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -72,5 +76,70 @@ public class MainActivity extends AppCompatActivity {
                 }
 
         });
+
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference readRef=FirebaseDatabase.getInstance().getReference().child("Student").child("Std1");
+                readRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChildren()){
+                            txtID.setText(dataSnapshot.child("id").getValue().toString());
+                            txtName.setText(dataSnapshot.child("name").getValue().toString());
+                            txtAdd.setText(dataSnapshot.child("address").getValue().toString());
+                            txtConNo.setText(dataSnapshot.child("conNo").getValue().toString());
+                        }
+                        else
+                            Toast.makeText(getApplicationContext(),"No Source to Display",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DatabaseReference upRef = FirebaseDatabase.getInstance().getReference().child("Student");
+                upRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChild("Std1")){
+                            try{
+                                std.setId(txtID.getText().toString().trim());
+                                std.setName(txtName.getText().toString().trim());
+                                std.setAddress(txtAdd.getText().toString().trim());
+                                std.setCuNo(Integer.parseInt(txtConNo.getText().toString().trim()));
+
+                                dbRef = FirebaseDatabase.getInstance().getReference().child("Student").child("Std1");
+                                dbRef.setValue(std);
+                                clearControls();
+
+                                Toast.makeText(getApplicationContext(),"Data Updated Successfully",Toast.LENGTH_SHORT).show();
+                            }
+                            catch(NumberFormatException e){
+                                Toast.makeText(getApplicationContext(),"Invalid Contact Number",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else
+                            Toast.makeText(getApplicationContext(),"No source to Update",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+        });
     }
+
 }
